aa3e5ff5feb299228c064711de6d45a1948c07f7
Avraham Appel <aappel248@gmail.com>
2024-12-02 16:19:56 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+    fenix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+        shuttle = naersk'.buildPackage rec {
+          version = "0.48.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "shuttle-hq";
+            repo = "shuttle";
+            rev = "v${version}";
+            hash = "sha256-PjPxx1Dmpv+ibXl2n6+yz0myOxvNtmtEkRmx7eR+dts=";
+          };
+        };
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alejandra
+            rust-analyzer
+            shuttle
+            (pkgs.fenix.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+          ];
+        };
+      }
+    );
+}
