ce4a9d58ba1a02cea0a7667d166aff151ac20ceb
Avraham Appel <aappel248@gmail.com>
2024-12-03 11:52:08 -0500

    Day -1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::http::header;
+use actix_web::{get, web::ServiceConfig, HttpResponse};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
-async fn hello_world() -> &'static str {
+async fn hello_bird() -> &'static str {
     "Hello, bird!"
+}
+
+#[get("/-1/seek")]
+async fn rick_roll() -> HttpResponse {
+    HttpResponse::Found()
+        .insert_header((
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        ))
+        .finish()
 }
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_bird).service(rick_roll);
     };
 
     Ok(config.into())
