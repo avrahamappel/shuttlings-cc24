8bf071f608508531ac224a8a56462ba7d575f4af
Avraham Appel <aappel248@gmail.com>
2024-12-03 07:54:35 -0500

    install cch24-validator

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         naersk' = pkgs.callPackage naersk {};
 
         shuttle = naersk'.buildPackage rec {
+          pname = "shuttle";
           version = "0.49.0";
           src = pkgs.fetchFromGitHub {
             owner = "shuttle-hq";
@@ -31,6 +32,17 @@
             hash = "sha256-97AiRgTPzkNsnxMTF0zleHVR6QYLyRlhguh2nz+duUM=";
           };
         };
+
+        cch24-validator = naersk'.buildPackage rec {
+          pname = "cch24-validator";
+          version = "2.0.1";
+          src = pkgs.fetchzip {
+            url = "https://crates.io/api/v1/crates/${pname}/${version}/download";
+            hash = "sha256-AdKFVGvRe3xG3bzaaAJ95NSWjJ4oCgmB8Y7UcaDVOiM=";
+            extension = "tar";
+          };
+        };
+
       in rec {
         defaultPackage = naersk'.buildPackage {
           src = ./.;
@@ -41,6 +53,7 @@
             alejandra
             rust-analyzer
             shuttle
+            cch24-validator
             (pkgs.fenix.stable.withComponents [
               "cargo"
               "clippy"
